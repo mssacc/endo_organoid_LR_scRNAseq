6a252e6f546ea67c03a8182e5c078ce2df0b20be
Michaela Sacco <132881041+mssacc@users.noreply.github.com>
2025-08-20 12:27:00 +1000

    Update and rename deseq2_DEI_with_bec.R

--- Rscripts/deseq2_DEI_with_bec.R
+++ Rscripts/deseq2_DEI_with_bec.R
@@ -39,15 +39,15 @@ group_metadata <- group_metadata %>%
 
 #Step 3 - apply limma batch effect correction
 apply_limma_batch_correction <- function(counts_matrix, batch_info) {
-  # Convert counts to log2 scale
+  #Convert counts to log2 scale
   log_counts <- log2(counts_matrix + 1)
-  # Create design matrix
+  #Create design matrix
   design <- model.matrix(~fertility, data = batch_info)
-  # Apply limma batch correction
+  #Apply limma batch correction
   corrected_counts <- removeBatchEffect(log_counts,
                                         batch = batch_info$batch,
                                         design = design)
-  # Convert back to counts scale
+  #Convert back to counts scale
   corrected_counts <- 2^corrected_counts - 1
   corrected_counts[corrected_counts < 0] <- 0
   
@@ -69,7 +69,7 @@ dds <- dds[keep,]
 dds <- DESeq(dds)
 
 
-#Step 5 - Extract DETs between Infertile and Fertile
+#Step 5 - Extract DEIs between Infertile and Fertile
 res <- results(dds, contrast = c("fertility", "Infertile", "Fertile"))
 
 #Make transcript name
@@ -80,31 +80,31 @@ res <- res %>%
 #Save DE analysis
 write.csv(res, "/data/gpfs/projects/punim1901/flames_v2/seurat_workspace/DETs/bulk_DE_analysis.csv", row.names = FALSE)
 
-#Step 6 - Filter and plot significant DETs
+#Step 6 - Filter and plot significant DEIs
 sig_res <- res %>%
   filter(padj < 0.05, abs(log2FoldChange) > 0.585)
 
 
 ###Volcano plot
-#Define colours for volcano plot
-keyvals <- ifelse(
-  res$log2FoldChange < -0.585 & res$padj < 0.05, '#F8766D',  
-  ifelse(res$log2FoldChange > 0.585 & res$padj < 0.05, 'palegreen3',  
-         '#717171'))  
-keyvals[is.na(keyvals)] <- '#717171'
-keyvals[is.na(keyvals)] <- '#717171'
-names(keyvals)[keyvals == 'palegreen3'] <- 'Upregulated'
-names(keyvals)[keyvals == '#F8766D'] <- 'Downregulated'
-names(keyvals)[keyvals == '#717171'] <- 'Not Significant'
-#Generate plot
-EnhancedVolcano(res, lab = rownames(res),
-                x = 'log2FoldChange', y = 'padj',
-                pCutoff = 0.05, FCcutoff = 1,
-                pointSize = 2, labSize = 4, axisLabSize = 14,  
-                title = "Bulk DETs - Infertile vs Fertile", titleLabSize = 25,
-                subtitle = NULL,
-                colCustom = keyvals,
-                legendPosition = 'right')
+  #Define colours for volcano plot
+  keyvals <- ifelse(
+    res$log2FoldChange < -0.585 & res$padj < 0.05, '#F8766D',  
+    ifelse(res$log2FoldChange > 0.585 & res$padj < 0.05, 'palegreen3',  
+           '#717171'))  
+  keyvals[is.na(keyvals)] <- '#717171'
+  keyvals[is.na(keyvals)] <- '#717171'
+  names(keyvals)[keyvals == 'palegreen3'] <- 'Upregulated'
+  names(keyvals)[keyvals == '#F8766D'] <- 'Downregulated'
+  names(keyvals)[keyvals == '#717171'] <- 'Not Significant'
+  #Generate plot
+  EnhancedVolcano(res, lab = rownames(res),
+                  x = 'log2FoldChange', y = 'padj',
+                  pCutoff = 0.05, FCcutoff = 1,
+                  pointSize = 2, labSize = 4, axisLabSize = 14,  
+                  title = "Bulk DEIs - Infertile vs Fertile", titleLabSize = 25,
+                  subtitle = NULL,
+                  colCustom = keyvals,
+                  legendPosition = 'right')
 
 res$negLogP <- -log10(res$padj)
 ggplot(res, aes(x = log2FoldChange, y = negLogP)) +
@@ -113,39 +113,36 @@ ggplot(res, aes(x = log2FoldChange, y = negLogP)) +
   theme_minimal()
 
 
-#Save DETs
+#Save DEIs
 write.csv(sig_res, "/data/gpfs/projects/punim1901/flames_v2/seurat_workspace/DETs/bulk_DETs.csv", row.names = FALSE)
 
 
-#Step 7 - Run plot
-#VST for visualization
-vsd <- vst(dds, blind = TRUE)
-# Extract the transformed expression matrix
-vsd_matrix <- assay(vsd)
-# Transpose for PCA (samples as rows, genes as columns -> must flip for prcomp)
-pca <- prcomp(t(vsd_matrix))
-# Run PCA on corrected VST
-pca_df <- as.data.frame(pca$x)
-pca_df$group_id <- rownames(pca_df)
-#Add metadata
-pca_df <- pca_df %>%
-  left_join(group_metadata, by = "group_id")
+#Step 7- Run plot
+  #VST for visualization
+    vsd <- vst(dds, blind = TRUE)
+  #Extract the transformed expression matrix
+    vsd_matrix <- assay(vsd)
+  #Transpose for PCA (samples as rows, genes as columns -> must flip for prcomp)
+    pca <- prcomp(t(vsd_matrix))
+  #Run PCA on corrected VST
+    pca_df <- as.data.frame(pca$x)
+    pca_df$group_id <- rownames(pca_df)
+  #Add metadata
+    pca_df <- pca_df %>%
+      left_join(group_metadata, by = "group_id")
 
 
 #Step 8 - Plot PCA
 ggplot(pca_df, aes(x = PC1, y = PC2, color = fertility, shape = cell_type)) +
   geom_point(size = 3, alpha = 0.8) +
   theme_minimal(base_size = 14) +
-  labs(
-    title = "Pseudobulk PCA (fertility)",
-    x = paste0("PC1 (", round(summary(pca)$importance[2, 1] * 100, 1), "%)"),
-    y = paste0("PC2 (", round(summary(pca)$importance[2, 2] * 100, 1), "%)"))
+  labs(title = "Pseudobulk PCA (fertility)",
+        x = paste0("PC1 (", round(summary(pca)$importance[2, 1] * 100, 1), "%)"),
+        y = paste0("PC2 (", round(summary(pca)$importance[2, 2] * 100, 1), "%)"))
 
 ggplot(pca_df, aes(x = PC1, y = PC2, color = batch, shape = cell_type)) +
   geom_point(size = 3, alpha = 0.8) +
   theme_minimal(base_size = 14) +
-  labs(
-    title = "Pseudobulk PCA (batch)",
-    x = paste0("PC1 (", round(summary(pca)$importance[2, 1] * 100, 1), "%)"),
-    y = paste0("PC2 (", round(summary(pca)$importance[2, 2] * 100, 1), "%)"))
-
+  labs(title = "Pseudobulk PCA (batch)",
+        x = paste0("PC1 (", round(summary(pca)$importance[2, 1] * 100, 1), "%)"),
+        y = paste0("PC2 (", round(summary(pca)$importance[2, 2] * 100, 1), "%)"))
